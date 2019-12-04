4b15102b2b51fba10c5d3cda462c9e9890eed93b
ramongallego <ramongallego@gmail.com>
2019-12-03 16:38:30 -0800

    Merging the repo with the R package structure

---
+++ R/hello.R
@@ -0,0 +1,23 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+## install if needed (do this exactly once):
+## install.packages("usethis")
+
+library(usethis)
+use_git_config(user.name = "ramongallego", user.email = "ramongallego@gmail.com")
