2cd26eb356752330a4016743112041f884fe2cc0
ramongallego <ramongallego@gmail.com>
2023-03-08 11:04:38 +0100

    fasta writer

---
+++ R/write_fasta.R
@@ -0,0 +1,7 @@
+fasta.writer <- function(df, sequence, header, file.out){
+  fasta <- character(nrow(df) * 2)
+  fasta[c(TRUE, FALSE)] <- paste0(">", df %>% pull({{header}}))
+  fasta[c(FALSE, TRUE)] <- df %>% pull({{sequence}})
+  writeLines(fasta, file.out)
+}
+
