fb1e80edc5a57600d83b698f703e85ffe0ed7026
ramongallego <ramongallego@gmail.com>
2024-07-09 14:59:20 +0200

    documentation

--- R/data.wrangling.r
+++ R/data.wrangling.r
@@ -15,3 +15,18 @@ tally_wide <- function (tibble, rows, cols, wt = NULL){
   
 }
 
+tibbleseqs <- function(fastafile){
+  insect::readFASTA(fastafile) -> temp
+  tibble (header = str_remove(names(temp),";$"),
+          seq = insect::dna2char(temp))
+}
+
+fasta_to_ASV_table <- function(path_to_fastas){
+  
+  map(path_to_fastas, tibbleseqs) |> 
+    set_names(basename(path_to_fastas)) |> 
+    bind_rows(.id="sample_name") |> 
+    separate(header, into = c("Hash", "nReads"), sep = ";size=", convert=T)
+    
+  
+}
