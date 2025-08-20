57a4fa092e368ec87ffe087314b0967b8f180bef
ramongallego <ramongallego@gmail.com>
2025-08-20 14:33:59 +0200

    molarity conversion functions

--- R/molarityconversions.R
+++ R/molarityconversions.R
@@ -1,8 +1,40 @@
+#' functions to translate mass into molarity and viceversa, given we are talking about double stranded DNA
+#' it requires two inputs, the mass (or molarity) and the length of the amplicon
+#' It works with the two most common concentrations used in Molecular Ecology labs
+#'  ng/ul for mass
+#'  nM for molarity
+
+#'
+#' @param ng Numeric. the concentration in ng per uL 
+#' @param length_amplicon Integer. The length of the amplicon in bp.
+#' 
+#' 
+#' @return Numeric. The equivalent concentration in nmoles per Litre
+#' @export
+#'
+#' @examples
+#' data("molarity.data")
+#' molarity.data %>% mutate(moles = ng2nM(ng=mass, length_amplicon = Amp_len))
+
+
 ng2nM <- function(ng, length_amplicon){
 x <- (ng*1000000)/(660*length_amplicon)
 return(x)
 }
+
+#' @param nM Numeric. The concentration in nmoles per Litre
+#' @param length_amplicon Integer. The length of the amplicon in bp.
+#' 
+#' @return Numeric. The equivalent concentration in ng per uL 
+#' @export
+#'
+#' @examples
+#' data("molarity.data")
+#' molarity.data %>% mutate(mass = nM2ng(nM=Molarity, length_amplicon = Amp_len))
+#' 
 nM2ng  <- function(nM, length_amplicon){
   x <- (nM*660*length_amplicon)/(1000000)
   return(x)
 }
+
+
