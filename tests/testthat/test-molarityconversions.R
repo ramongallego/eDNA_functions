8fe7d0a99ba983b163e3f3185a1916a13d4aa75e
ramongallego <ramongallego@gmail.com>
2025-08-20 16:46:29 +0200

    molarity tests

--- tests/testthat/test-molarityconversions.R
+++ tests/testthat/test-molarityconversions.R
@@ -9,3 +9,8 @@ test_that("output is numeric", {
   expect_type(ng2nM(5, 150), "double")
   expect_type(nM2ng(10, 150), "double")
 })
+test_that("consistency",{
+  length_amp <- 300
+  mass <- 10
+  expect_equal(nM2ng(ng2nM(mass, length_amplicon = length_amp), length_amplicon = length_amp), mass)
+})
