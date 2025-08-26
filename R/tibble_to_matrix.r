58f4759c72322fad716b17024288c35ccdcf496d
ramongallego <ramongallego@gmail.com>
2025-08-26 10:01:02 +0200

    tibble to vegan

--- R/tibble_to_matrix.r
+++ R/tibble_to_matrix.r
@@ -1,177 +1,139 @@
-# A function that takes a long dataframe and turns it into a distance matrix
+#' Convert a long tibble to a community matrix
+#'
+#' Converts a long-format table of sequence counts into a wide community
+#' matrix (samples × taxa) suitable for vegan or other community ecology tools.
+#'
+#' @param long.table A tibble with at least sample, taxon, and abundance columns.
+#' @param taxon Column containing taxa/OTU IDs (unquoted).
+#' @param Abundance Column with counts/abundance values (unquoted).
+#' @param sample.name Column with sample IDs (unquoted).
+#'
+#' @importFrom rlang enquo !!
+#' @importFrom tidyr replace_na pivot_wider
+#' @importFrom dplyr mutate select pull arrange
+#' @importFrom tidyselect all_of
+#' @importFrom vegan decostand vegdist
+#' 
+#' @return A numeric matrix with taxa as columns and samples as row names.
+#' @export
+#' @rdname tibble_to_vegan
+#'
+#' @examples
+#' tibble_to_comm(my_data, taxon = Species, Abundance = Count, sample.name = SampleID)
 
-tibble_to_comm <- function (long.table,taxon,Abundance, sample.name){
-  sample.name = rlang::enquo(sample.name)
-  taxon = rlang::enquo(taxon) # quote taxon so it works with select
-  Abundance = rlang::enquo(Abundance)
+tibble_to_comm <- function(long.table, taxon, Abundance, sample.name) {
+  sample.name <- rlang::enquo(sample.name)
+  taxon <- rlang::enquo(taxon)
+  Abundance <- rlang::enquo(Abundance)
   
-  long.table %>% 
-    mutate(!!taxon := case_when(is.na(!!taxon) ~ "NA",
-                                TRUE           ~ as.character(!!taxon))) -> long.table
-  
-  cols <- long.table %>%
-    ungroup %>%
-    dplyr::select(!!taxon) %>% 
-    distinct() %>% pull() # capture the unique values of the taxa
+  long.table <- long.table %>%
+    mutate(!!taxon := tidyr::replace_na(as.character(!!taxon), "NA"))
   
   
+  taxa <- long.table %>% pull(!!taxon) %>% unique()
   
-  long.table %>%
-    ungroup %>% 
-    select(!!taxon, !!Abundance, !!sample.name) %>% # select at least three columns: nReads, taxa and the sampleID
-    pivot_wider (names_from = !!taxon, values_from = !!Abundance, values_fill = 0) -> matrix_1
+  matrix_1 <- long.table %>%
+    select(!!sample.name, !!taxon, !!Abundance) %>%
+    tidyr::pivot_wider(names_from = !!taxon,
+                       values_from = !!Abundance,
+                       values_fill = 0)
   
-  samples <- pull (matrix_1, !!sample.name)
-  dplyr::select (matrix_1, cols) -> matrix_1 # select only the spp
-  data.matrix(matrix_1) -> matrix_1 # make it a data matrix
+  samples <- pull(matrix_1, !!sample.name)
   
-  dimnames(matrix_1)[[1]] <- samples
-  
+  matrix_1 <- select(matrix_1, all_of(taxa)) %>% data.matrix()
+  rownames(matrix_1) <- samples
   
   return(matrix_1)
 }
 
 
 
-tibble_to_matrix <- function (long.table,taxon,Abundance, sample.name, distance = "bray", transformation, ...) {
+#' Convert long tibble to distance matrix
+#'
+#' Produces a distance matrix between samples using vegan's \code{vegdist()}.
+#' Optionally applies a data transformation before distance calculation.
+#'
+#' @param long.table A tibble with sample, taxon, and abundance columns.
+#' @param taxon Column containing taxa/OTU IDs (unquoted).
+#' @param Abundance Column with counts/abundance values (unquoted).
+#' @param sample.name Column with sample IDs (unquoted).
+#' @param distance Distance metric to use (default = "bray").
+#' @param transformation Optional transformation (e.g. "hellinger", "log").
+#' @param ... Extra metadata columns to keep alongside.
+#'
+#' @return A \code{dist} object.
+#' @export
+#' @rdname tibble_to_vegan
+#' 
+#' @examples
+#' tibble_to_dist(my_data, taxon = Species, Abundance = Count, sample.name = SampleID,
+#'                distance = "bray", transformation = "hellinger")
+tibble_to_dist <- function(long.table, taxon, Abundance, sample.name,
+                           distance = "bray", transformation = NULL, ...) {
   
-  sample.name = rlang::enquo(sample.name)
-  taxon = rlang::enquo(taxon) # quote taxon so it works with select
-  choice = rlang::enquo(distance)
-  Abundance = rlang::enquo(Abundance)
+  sample.name <- rlang::enquo(sample.name)
+  taxon <- rlang::enquo(taxon)
+  Abundance <- rlang::enquo(Abundance)
   
+  long.table <- long.table %>%
+    mutate(!!taxon := if_else(is.na(!!taxon), "NA", as.character(!!taxon)))
   
-  # So now the eDNA index works let's see if we can use decostand
+  taxa <- long.table %>% pull(!!taxon) %>% unique()
   
-  # In case there are NAs in teh taxa, the thing breaks - 
+  wide_tibble <- long.table %>%
+    select(!!sample.name, !!taxon, !!Abundance, ...) %>%
+    tidyr::pivot_wider(names_from = !!taxon,
+                       values_from = !!Abundance,
+                       values_fill = 0) %>%
+    arrange(!!sample.name)
   
-  ## 
+  samples <- pull(wide_tibble, !!sample.name)
   
-  long.table %>% 
-    mutate(!!taxon := case_when(is.na(!!taxon) ~ "NA",
-                                TRUE           ~ as.character(!!taxon))) -> long.table
+  spp_matrix <- select(wide_tibble, all_of(taxa)) %>% data.matrix()
+  rownames(spp_matrix) <- samples
   
-  cols <- long.table %>% ungroup %>% dplyr::select(!!taxon) %>% distinct() %>% pull() # capture the unique values of the taxa
-  
-  
-   
-  long.table %>%
-    ungroup %>% 
-    select(!!taxon, !!Abundance, !!sample.name, ...) %>% # select at least three columns: nReads, taxa and the sampleID
-    spread (key = !!taxon, value = !!Abundance, fill = 0) -> wide_tibble # Make a wide table, like vegan likes
-   wide_tibble %>% arrange(!!sample.name) -> wide.rearranged
-  #  return(wide_tibble$Sample_name) 
-  # return (head(wide.rearranged))
-  sample.col <- colnames(wide_tibble)[!colnames(wide_tibble) %in% cols] # Which column has all the sample info
-  #return(env)
-  dplyr::select (wide.rearranged, sample.col) -> samples # The values of the sample info in the order they appear in the wide table
-  # return(samples)
-  samples <- pull (wide.rearranged, !!sample.name)
- # return(samples)
-  dplyr::select (wide.rearranged, cols) -> wide_spp # select only the spp
-   #return(wide_spp)
-  data.matrix(wide_spp) -> matrix_1 # make it a data matrix
-   #return(str(matrix_1))
-  dimnames(matrix_1)[[1]] <- samples # name it with the sample info
-   #return(str(matrix_1))
-  # Now that the matrix is ready to go, we can apply decostand if needed
-  
-  good.transformations <- c("total", "max", "frequency", "normalize", "range", 
-                            "rank", "rrank", "standardize", "pa", "chi.square", "hellinger", 
-                            "log")
-  if (transformation == "sq") {
-    sqrt(matrix_1) -> matrix_1
-  }else{
-  if(transformation %in%  good.transformations ){
-    decostand(matrix_1, method = transformation) -> matrix_1
-  }
-  }
-  #simper(matrix_1, env$Site)
-  vegdist(matrix_1, method = distance) -> matrix_1 
-  return(matrix_1)
-  # calculate the dissimilarity matrix
-  
-#   adonis(matrix_1 ~  Site*Date, data= env)
-  # as.tibble(subset(melt(as.matrix(matrix_1))))
-}
-
-tibble_to_permanova <- function (long.table,taxon,Abundance, sample.name, distance = "bray", transformation, formula = "",...) {
-  
-  sample.name = rlang::enquo(sample.name)
-  taxon = rlang::enquo(taxon) # quote taxon so it works with select
-  choice = rlang::enquo(distance)
-  Abundance = rlang::enquo(Abundance)
-  
-  
-  # So now the eDNA index works let's see if we can use decostand
-  
-  
-  
-  cols = long.table %>% ungroup %>% dplyr::select(!!taxon) %>% distinct() %>% pull() # capture the unique values of the taxa
-  
-  
-  long.table %>%
-    ungroup %>% 
-    select(!!taxon, !!Abundance, !!sample.name, ...) %>% # select at least three columns: nReads, taxa and the sampleID
-    spread (key = !!taxon, value = !!Abundance, fill = 0) -> matrix_1 # Make a wide table, like vegan likes
-  
-  env <- colnames(matrix_1)[!colnames(matrix_1) %in% cols] # Which column has all the sample info
-  
-  dplyr::select (matrix_1, env) -> env # The values of the sample info in the order they appear in the wide table
-  
-  samples <- pull (matrix_1, !!sample.name)
-  dplyr::select (matrix_1, cols) -> matrix_1 # select only the spp
-  data.matrix(matrix_1) -> matrix_1 # make it a data matrix
-  
-  dimnames(matrix_1)[[1]] <- samples # name it with the sample info
-  
-  # Now that the matrix is ready to go, we can apply decostand if needed
-  
-  good.transformations <- c("total", "max", "frequency", "normalize", "range", 
-                            "rank", "rrank", "standardize", "pa", "chi.square", "hellinger", 
-                            "log")
-  if (transformation == "sq") {
-    sqrt(matrix_1) -> matrix_1
-  }else{
-    if(transformation %in%  good.transformations ){
-      decostand(matrix_1, method = transformation) -> matrix_1
+  # Apply transformation if requested
+  if (!is.null(transformation)) {
+    if (transformation == "sq") {
+      spp_matrix <- sqrt(spp_matrix)
+    } else if (transformation %in% vegan::decostand.methods) {
+      spp_matrix <- vegan::decostand(spp_matrix, method = transformation)
     }
   }
-  #simper(matrix_1, env$Site)
-  vegdist(matrix_1, method = distance) -> matrix_1 
-  return(matrix_1)
-  # calculate the dissimilarity matrix
   
-     adonis(matrix_1 ~  Site*Date, data= env)
-  # as.tibble(subset(melt(as.matrix(matrix_1))))
+  return(vegan::vegdist(spp_matrix, method = distance))
 }
 
 
-tibble_to_env <- function (long.table,taxon,Abundance,sample.name, ...) {  # A function that returns the accompaining environmental matrix  
+
+
+#' Extract environmental metadata from a long tibble
+#'
+#' Returns the environmental data frame (sample metadata) from a long
+#' sequence/OTU tibble by removing taxon and abundance columns.
+#'
+#' @param long.table A tibble with sample, taxon, abundance, and metadata columns.
+#' @param taxon Column containing taxa/OTU IDs (unquoted).
+#' @param Abundance Column with counts/abundance values (unquoted).
+#' @param sample.name Column with sample IDs (unquoted).
+#' @param ... Additional metadata columns to retain.
+#'
+#' @return A tibble of unique sample-level metadata.
+#' @export
+#' @rdname tibble_to_vegan
+#' 
+#' @examples
+#' tibble_to_env(my_data, taxon = Species, Abundance = Count, sample.name = SampleID)
+
+tibble_to_env <- function(long.table, taxon, Abundance, sample.name, ...) {
+  taxon <- rlang::enquo(taxon)
+  Abundance <- rlang::enquo(Abundance)
+  sample.name <- rlang::enquo(sample.name)
   
-  # Enquos to allow me to play with the data
+  env <- long.table %>%
+    select(-!!taxon, -!!Abundance) %>%
+    distinct() %>%
+    arrange(!!sample.name)
   
-  sample.name = rlang::enquo(sample.name)
-  taxon = rlang::enquo(taxon) # quote taxon so it works with select
-  #choice = rlang::enquo(distance)
-  Abundance = rlang::enquo(Abundance)
-  
-  cols <- long.table %>% ungroup %>% dplyr::select(!!taxon) %>% distinct() %>% pull() # capture the unique values of the taxa
-   #return(cols)
-  long.table %>%
-    ungroup %>% 
-    # select(!!taxon, !!Abundance, !!sample.name, ...) %>% 
-    select( -!!taxon, -!!Abundance) %>% 
-    distinct()-> wide_tibble
-    # spread (key = !!taxon, value = !!Abundance, fill = 0) -> wide_tibble
-  wide_tibble %>% arrange(!!sample.name) -> wide_tibble
-   # return (head(wide.rearranged))
-  env <- colnames(wide_tibble)[!colnames(wide_tibble) %in% cols]
-  # return(env)
-  dplyr::select (wide_tibble, env) -> env
-  env %>% distinct()-> env
   return(env)
- 
-  
 }
-
