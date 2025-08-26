c2980aea582a0036e395228eff370158ddb0b8c2
ramongallego <ramongallego@gmail.com>
2025-08-26 09:00:34 +0200

    external data for tests and test for cutadapt

--- tests/testthat/test-read_info_file.R
+++ tests/testthat/test-read_info_file.R
@@ -16,17 +16,17 @@ test_that("Illumina cutadapt info file is read correctly", {
 })
 
 
-test_that("Nanopore cutadapt info file is read correctly", {
-  path <- system.file("extdata", "cutadapt_info_nanopore.txt", package = "eDNAfuns")
-  df <- read_info_file_nanopore(path)
-  
-  # Basic structure
-  expect_s3_class(df, "tbl_df")
-  expect_true(all(c("Seq.id", "n_errors", "start_adap", "end_adap", "matching_seq") %in% names(df)))
-  
-  # Nanopore file has expected number of columns
-  expect_equal(ncol(df), 14)
-  
-  # QScores columns exist and are character
-  expect_type(df$QScores_matching, "character")
-})
+# test_that("Nanopore cutadapt info file is read correctly", {
+#   path <- system.file("extdata", "cutadapt_info_nanopore.txt", package = "eDNAfuns")
+#   df <- read_info_file_nanopore(path)
+#   
+#   # Basic structure
+#   expect_s3_class(df, "tbl_df")
+#   expect_true(all(c("Seq.id", "n_errors", "start_adap", "end_adap", "matching_seq") %in% names(df)))
+#   
+#   # Nanopore file has expected number of columns
+#   expect_equal(ncol(df), 14)
+#   
+#   # QScores columns exist and are character
+#   expect_type(df$QScores_matching, "character")
+# })
