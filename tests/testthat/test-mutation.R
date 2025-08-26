ed666086965aa3e5a4b9f00b99812ffcc1d155fb
ramongallego <ramongallego@gmail.com>
2025-08-26 14:11:42 +0200

    getting ready for CRAN

--- tests/testthat/test-mutation.R
+++ tests/testthat/test-mutation.R
@@ -4,14 +4,14 @@ library(insect)  # if needed for your data
 
 context("mutation function works as expected")
 
-char2dna(list(seq1 = "TAACGC", seq2 = "ATTGCG")) -> newwoodmouse
-dna2char(newwoodmouse) -> wood_chars
-mutated_bin  <- mutation(newwoodmouse, format = "bin", n.mutations = 1)
+char2dna(list(seq1 = "TAACGC", seq2 = "ATTGCG")) -> test_seq
+dna2char(test_seq) -> wood_chars
+mutated_bin  <- mutation(test_seq, format = "bin", n.mutations = 1)
 mutated_char <- mutation(wood_chars, format = "char", n.mutations = 1)
 # Test: output has same length as input
 test_that("output length matches input length with many seqs in input", {
   
-  dna2char(newwoodmouse) |> enframe() -> comparable_woodmouse
+  dna2char(test_seq) |> enframe() -> comparable_woodmouse
   dna2char(mutated_bin) |> enframe() -> comparable_output
   
   expect_equal(nchar(comparable_woodmouse$value), nchar(comparable_output$value))
@@ -26,7 +26,7 @@ test_that("output length matches input length in chars with many seqs in input",
 # Test: mutated positions differ from original
 test_that("mutated sequences differ from original", {
  
-  differences <- mapply(function(orig, mut) any(orig != mut), newwoodmouse, mutated_bin)
+  differences <- mapply(function(orig, mut) any(orig != mut), test_seq, mutated_bin)
   expect_true(all(differences))
 })
 test_that("mutated sequences differ from original in chars", {
@@ -38,8 +38,8 @@ test_that("mutated sequences differ from original in chars", {
 
 # Test: error when both or neither mutation arguments are provided
 test_that("error triggers with invalid arguments", {
-  expect_error(mutation(newwoodmouse, format = "bin", n.mutations = NA, prob.mutation = NA))
-  expect_error(mutation(newwoodmouse, format = "bin", n.mutations = 2, prob.mutation = 0.1))
+  expect_error(mutation(test_seq, format = "bin", n.mutations = NA, prob.mutation = NA))
+  expect_error(mutation(test_seq, format = "bin", n.mutations = 2, prob.mutation = 0.1))
   expect_error(mutation(wood_chars, format = "char", n.mutations = NA, prob.mutation = NA))
   expect_error(mutation(wood_chars, format = "char", n.mutations = 2, prob.mutation = 0.1))
 })
@@ -57,3 +57,4 @@ test_that("output is character class", {
 
   expect_type(mutated_char, "character")
 })
+# 
